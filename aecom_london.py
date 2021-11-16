d4fd60de60d7aec59559347c9d6db23eea0511a3
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-16 01:26:39 +0000

    Update aecom_london.py

--- aecom_london.py
+++ aecom_london.py
@@ -1,4 +1,18 @@
+####################Preparation#######################
+
 import pandas as pd
 import streamlit as st
-df = pd.read_csv('london.csv')
-st.dataframe(df)
+
+
+
+####################Simulation#######################
+
+
+class Station(object): # Define the fire station class
+  def __init__(self, env, num_engines, station_name):
+    self.env = env
+    self.name = station_name
+    self.engines = simpy.Resource(env, num_engines)
+
+  def mobilisation(self, fire_incident): # Define the
+    yield self.env.timeout(generate_attendence_time(self.name)/60)
