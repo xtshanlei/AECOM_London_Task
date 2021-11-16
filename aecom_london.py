39c684b369dd0bd9c72ab3edbb3067f7fdb2b9e4
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-16 01:47:27 +0000

    Update aecom_london.py

--- aecom_london.py
+++ aecom_london.py
@@ -2,13 +2,26 @@
 
 import pandas as pd
 import streamlit as st
+from geopy.geocoders import GoogleV3
+locator = GoogleV3(api_key=st.secrets['google_api'])
+@st.cache
+def convert_add(add):
+  location = locator.geocode(add)
+  return location.latitude, location.longitude
 
 st.title('London Fire Station Project - AECOM')
-df = pd.read_csv('london.csv')
+clean_df = pd.read_csv('london.csv')
 st.write(df['IncidentNumber'][0])
 
 ####################Simulation#######################
+def generate_attendence_time(station): # Generate random attendence time based on historical data
+  mu,sigma = clean_df[clean_df['DeployedFromStation_Name']==station]['AttendanceTimeSeconds'].describe()['mean'],clean_df[clean_df['DeployedFromStation_Name']==station]['AttendanceTimeSeconds'].describe()['std']
+  return abs(np.random.normal(mu, sigma, 1))
 
+def generate_incident_time_gap(station): # Generate random fire incident for a certain station
+  incident_time_gap = clean_df[clean_df['DeployedFromStation_Name']==station].sort_values(by='DateAndTimeMobilised').reset_index()['DateAndTimeMobilised'].diff().reset_index()
+  mu, sigma = incident_time_gap['DateAndTimeMobilised'][1:].dt.total_seconds().div(60).astype(int).describe()['mean'],incident_time_gap['DateAndTimeMobilised'][1:].dt.total_seconds().div(60).astype(int).describe()['std']
+  return abs(np.random.normal(mu, sigma, 1))
 
 class Station(object): # Define the fire station class
   def __init__(self, env, num_engines, station_name):
@@ -16,5 +29,43 @@ class Station(object): # Define the fire station class
     self.name = station_name
     self.engines = simpy.Resource(env, num_engines)
 
-  def mobilisation(self, fire_incident): # Define the
+  def mobilisation(self, fire_incident): # Simulate the turnout process based on each station's attendence time
     yield self.env.timeout(generate_attendence_time(self.name)/60)
+
+def Dealing_with_Fire(env, fire_incident, station): # Dealing with the fire incident for each station based on the attendence time
+  # Fire incidents occurs
+  occur_time = env.now
+  st.write('Fire occurs at {} hour'.format(occur_time/60))
+  with station.engines.request() as request:
+    yield request
+    yield env.process(station.mobilisation(fire_incident))
+  # Fire turned out
+  wait_times.append(float(env.now - occur_time))
+  st.write('Fire turned out at {} hour, the whole process took {} minutes'.format(env.now/60,float(env.now - occur_time)))
+
+def run_station(env, num_engines, station_name): # Define the station running
+  station = Station(env, num_engines, station_name)
+  fire_incident=1
+  env.process(Dealing_with_Fire(env, fire_incident, station))
+
+  while True:
+    yield env.timeout(generate_incident_time_gap(station.name))  # Wait a bit before generating a new fire incident
+    fire_incident += 1
+    env.process(Dealing_with_Fire(env, fire_incident, station))
+
+def get_average_wait_time(wait_times): # Calculate average wait time
+  average_wait = statistics.mean(wait_times)
+  # Pretty print the results
+  minutes, frac_minutes = divmod(average_wait, 1)
+  seconds = frac_minutes * 60
+  return round(minutes), round(seconds)
+
+
+wait_times = []
+random.seed(42)
+station = st.selectbox('Please select the fire station you want to simulate',(clean_df['DeployedFromStation_Name'].unique()))
+num_engines= st.slider('Please choose the number of fire engines',1,50,1)
+# Run the simulation
+env = simpy.Environment()
+env.process(run_station(env, num_engines,'Soho'))
+env.run(until=60*24*7)
