ffca8ae29bc5b49c72b0d2e91ee178b7980653fe
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-16 02:02:42 +0000

    Update aecom_london.py

--- aecom_london.py
+++ aecom_london.py
@@ -16,10 +16,11 @@ st.title('London Fire Station Project - AECOM')
 clean_df = pd.read_csv('london.csv')
 
 ####################Visualisation#######################
+# Anually
 annual_incidents_per_station= clean_df.groupby(['DeployedFromStation_Name','CalYear']).nunique()['IncidentNumber'].reset_index() # Average number of incidents for each station for each year
 avg_annual_incidents_per_station = annual_incidents_per_station.groupby(['DeployedFromStation_Name']).mean()['IncidentNumber'].reset_index() # Average number of incidents for each station
 
-#Convert fire station into coordinates using Google API
+# Convert fire station into coordinates using Google API
 lat_lst = []
 lon_lst = []
 for add in avg_annual_incidents_per_station['DeployedFromStation_Name']:
@@ -38,9 +39,25 @@ fig = px.density_mapbox(avg_annual_incidents_per_station, lat='lat', lon='lon', 
                         labels = {'IncidentNumber':'No.of Incidents'},range_color=[0,5000])
 st.plotly_chart(fig)
 
-
-
-
+# Hourly
+hour_incidents_per_station = clean_df.groupby(['DeployedFromStation_Name','date','HourOfCall']).count()
+avg_hourly_incidents_per_station = hour_incidents_per_station.groupby(['DeployedFromStation_Name','HourOfCall']).mean()['IncidentNumber'].reset_index()
+hour_lat_list = []
+hour_lon_list = []
+for station in avg_hourly_incidents_per_station['DeployedFromStation_Name']:
+  lat = avg_annual_incidents_per_station[avg_annual_incidents_per_station['DeployedFromStation_Name']==station]['lat'].values[0]
+  lon = avg_annual_incidents_per_station[avg_annual_incidents_per_station['DeployedFromStation_Name']==station]['lon'].values[0]
+  hour_lat_list.append(lat)
+  hour_lon_list.append(lon)
+avg_hourly_incidents_per_station['lat']=hour_lat_list
+avg_hourly_incidents_per_station['lon']=hour_lon_list
+hour_fig = px.density_mapbox(avg_hourly_incidents_per_station, lat='lat', lon='lon', z='IncidentNumber', radius=20,
+                        center=dict(lat=convert_add('London')[0], lon=convert_add('London')[1]), zoom=7,
+                        mapbox_style="stamen-toner",hover_name='DeployedFromStation_Name',
+                        labels = {'IncidentNumber':'No.of Incidents'},
+                        range_color=[avg_hourly_incidents_per_station['IncidentNumber'].min(),avg_hourly_incidents_per_station['IncidentNumber'].max()],
+                        animation_frame='HourOfCall')
+st.plotly_chart(hour_fig)
 ####################Simulation#######################
 def generate_attendence_time(station): # Generate random attendence time based on historical data
   mu,sigma = clean_df[clean_df['DeployedFromStation_Name']==station]['AttendanceTimeSeconds'].describe()['mean'],clean_df[clean_df['DeployedFromStation_Name']==station]['AttendanceTimeSeconds'].describe()['std']
