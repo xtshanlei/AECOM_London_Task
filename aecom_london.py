1643fa3cd5b84c1c11b5a38acf05bcc39b28b985
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-16 02:21:37 +0000

    Update aecom_london.py

--- aecom_london.py
+++ aecom_london.py
@@ -17,6 +17,8 @@ clean_df = pd.read_csv('london.csv')
 
 ####################Visualisation#######################
 # Anually
+st.header('Annual Average Number of Fire Incidents for Each Station')
+st.text('The area with highest risk of fire incident is the centre of London. The busiest station is Soho')
 annual_incidents_per_station= clean_df.groupby(['DeployedFromStation_Name','CalYear']).nunique()['IncidentNumber'].reset_index() # Average number of incidents for each station for each year
 avg_annual_incidents_per_station = annual_incidents_per_station.groupby(['DeployedFromStation_Name']).mean()['IncidentNumber'].reset_index() # Average number of incidents for each station
 
@@ -40,6 +42,8 @@ fig = px.density_mapbox(avg_annual_incidents_per_station, lat='lat', lon='lon', 
 st.plotly_chart(fig)
 
 # Hourly
+st.header('Hourly Average Number of Fire Incidents for Each Station')
+st.text('The figure shows the average fire incidents each hour for each station. Hit the play button below or slide to choose the hour to explore more.')
 hour_incidents_per_station = clean_df.groupby(['DeployedFromStation_Name','date','HourOfCall']).count()
 avg_hourly_incidents_per_station = hour_incidents_per_station.groupby(['DeployedFromStation_Name','HourOfCall']).mean()['IncidentNumber'].reset_index()
 hour_lat_list = []
@@ -51,8 +55,8 @@ for station in avg_hourly_incidents_per_station['DeployedFromStation_Name']:
   hour_lon_list.append(lon)
 avg_hourly_incidents_per_station['lat']=hour_lat_list
 avg_hourly_incidents_per_station['lon']=hour_lon_list
-hour_fig = px.density_mapbox(avg_hourly_incidents_per_station, lat='lat', lon='lon', z='IncidentNumber', radius=20,
-                        center=dict(lat=convert_add('London')[0], lon=convert_add('London')[1]), zoom=7,
+hour_fig = px.density_mapbox(avg_hourly_incidents_per_station, lat='lat', lon='lon', z='IncidentNumber', radius=40,
+                        center=dict(lat=convert_add('London')[0], lon=convert_add('London')[1]), zoom=8,
                         mapbox_style="stamen-toner",hover_name='DeployedFromStation_Name',
                         labels = {'IncidentNumber':'No.of Incidents'},
                         range_color=[avg_hourly_incidents_per_station['IncidentNumber'].min(),avg_hourly_incidents_per_station['IncidentNumber'].max()],
